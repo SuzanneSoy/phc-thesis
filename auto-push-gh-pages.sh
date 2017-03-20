68af5bbfa14e321fb6aebaa06c8fd57ad5857663
Georges Dupéron <georges.duperon@gmail.com>
2017-03-20 18:59:07 +0100

    Fixed issues with deploy script

--- auto-push-gh-pages.sh
+++ auto-push-gh-pages.sh
@@ -5,8 +5,8 @@ if test "$(git config remote.origin.url)" != "https://github.com/jsmaniac/phc-th
   echo "Not on official repo, will not deploy gh-pages."
 elif test "$TRAVIS_PULL_REQUEST" != "false"; then
   echo "This is a Pull Request, will not deploy gh-pages."
-elif test "$TRAVIS_BRANCH" = "master"; then
-  echo "Not on master branch, will not deploy gh-pages."
+elif test "$TRAVIS_BRANCH" != "master"; then
+  echo "Not on master branch (TRAVIS_BRANCH = $TRAVIS_BRANCH), will not deploy gh-pages."
 elif test -z "${encrypted_1b66487e02e5_key:-}" -o -z "${encrypted_1b66487e02e5_iv:-}"; then
   echo "Travis CI secure environment variables are unavailable, will not deploy gh-pages."
 else
@@ -17,11 +17,13 @@ else
   git config --global user.email "(git log --format="%aE" HEAD -1)"
 
   # SSH configuration
+  set +x
   if openssl aes-256-cbc -K $encrypted_1b66487e02e5_key -iv $encrypted_1b66487e02e5_iv -in travis-deploy-key-id_rsa.enc -out travis-deploy-key-id_rsa -d >/dev/null 2>&1; then
     echo "Decrypted key successfully."
   else
     echo "Error while decrypting key."
   fi
+  set -x
   chmod 600 travis-deploy-key-id_rsa
   eval `ssh-agent -s`
   ssh-add travis-deploy-key-id_rsa
