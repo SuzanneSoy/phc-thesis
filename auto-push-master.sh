5c4fdc9b66e06e74046df5f78c459380e8c3be3c
Georges Dupéron <georges.duperon@gmail.com>
2017-03-21 19:45:13 +0100

    Fixed regexp

--- auto-push-master.sh
+++ auto-push-master.sh
@@ -35,7 +35,7 @@ else
 
   # Push to the auto-git branch, which can then auto-push to master, after this build finished
   repo_url="$(git config remote.origin.url)"
-  ssh_repo_url="$(echo "$repo_url" | sed -e 's|^https://github.com/|git@github.com:')"
+  ssh_repo_url="$(echo "$repo_url" | sed -e 's|^https://github.com/|git@github.com:|')"
   commit_hash="$(git rev-parse --short HEAD)"
   git log --oneline --decorate --graph -10
   git fetch auto-push
